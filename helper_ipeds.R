4e165cc22b4dd3413d50e7ca4d82f801e7685fb8
vpascal <vpascal2@gmail.com>
2017-06-27 22:57:32 -0400

    IPEDS

---
+++ helper_ipeds.R
@@ -0,0 +1,35 @@
+# this function helps to extract specific files from zipped folders in the IPEDS data archives.
+
+library(purrr)
+library(readr)
+
+extract_files <- function(x) {
+  temp <- map(.f = unzip, .x = x, list = TRUE)
+  files <- vector()
+  
+  for (i in temp) {
+    if (nrow(i) == 2) {
+      files <- rbind(files, i$Name[2])
+    } else {
+      files <- rbind(files, i$Name[1])
+    }
+  }
+  
+  files <- as.character(files)
+  
+  for (i in seq_along(files)) {
+    unzip(x[i], files = files[i], exdir = 'temp')
+  }
+  
+  final_files <- dir(path = "temp/",
+                     pattern = "csv",
+                     full.names = T)
+  temp <- map_df(.x = final_files, .f = read_csv)
+  temp
+  
+  
+}
+
+# examples
+# temp <- extract_files(files)
+# unlink("temp",recursive=TRUE)
