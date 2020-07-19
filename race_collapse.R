45d7833e5187fca92c5803dc089dbdd9a2957239
vpascal <vpascal2@gmail.com>
2020-07-19 14:00:11 -0400

    Update race_collapse.R

--- race_collapse.R
+++ race_collapse.R
@@ -1,10 +1,13 @@
-race_collape <- function(dataset) {
-  temp <- apply(apply(dataset, 2, function(x)
-    !is.na(x)),
+race_collapse1 <- function(race1){
+  
+  race1 <- purrr::map_df(race1, as.character)  
+  
+  temp <- apply(
+    apply(race1, 2, function(x) !is.na(x)),
     1, sum)
-  
-  temp[temp > 1] <- 'more than one'
-  temp[temp == 0] <- 'uknown'
-  
-  ifelse(temp == 1, dplyr::coalesce(!!!dataset), temp)
+ 
+  temp[temp>1] <- 'Two or More'
+  temp[temp==0] <- 'Unknown'
+ 
+  ifelse(temp==1,coalesce(!!!race1),temp)
 }
